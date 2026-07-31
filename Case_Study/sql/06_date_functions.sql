a8fa0b34ff4925cedbd1358afd59dc50855972b9
NombuleloDlamini <nombulelo.dlamini008@gmail.com>
2026-07-31 00:07:14 +0000

    creating tables inside databases

--- Case_Study/sql/06_date_functions.sql
+++ Case_Study/sql/06_date_functions.sql
@@ -1,12 +1,10 @@
 -- Databricks notebook source
 -- the system recognizes the date format as 'yyyy-MM-dd'
 
-
 SELECT CURRENT_DATE() AS today_date;  -- display current date
 SELECT CURRENT_TIMESTAMP() AS time; -- display current time
 
 -- converting string to read as a date
-
 SELECT TO_DATE('2026-07-31') AS date;
 
 -- converting a date to read as string/ Extracting parts of date
@@ -22,7 +20,6 @@ SELECT DATEDIFF('2026-07-30','2026-07-11') AS duration;
 -- DATE_ADD(date, number of days you want to add)
 -- putting a negative interger will subtract(date back)  OR use DATE_SUB()
 -- ADD_MONTHS will add number of months
-
 SELECT DATE_ADD('2026-07-11',20) AS new_date;
 
 
---
+++ Case_Study/sql/07_table_operations.sql
@@ -0,0 +1,31 @@
+-- Databricks notebook source
+-- creatind a database in databricks --
+CREATE CATALOG IF NOT EXISTS course_enrollment;
+
+USE CATALOG course_enrollment;
+
+-- CREATE SCHEMA IF NOT EXISTS students_hh;
+-- To drop is to delete schema --
+--==DROP SCHEMA students_hh;
+
+CREATE SCHEMA IF NOT EXISTS students;
+
+-- ceating a table in a database and given schema --
+CREATE OR REPLACE TABLE course_enrollment.students.profiles (
+    student_ID INT,
+    name STRING,
+    surname STRING,
+    age INT,
+    email STRING,
+    registration_dt DATE);
+
+INSERT INTO course_enrollment.students.profiles
+    VALUES
+        (101, 'Rochester', 'Jones', 28, 'roch@google.com', '2019-01-18'),
+        (102, 'Sive', 'Mgazi', 21, 'sive@gmail.com', '2022-01-28'),
+        (103, 'Estella', 'James', 24, 'stellaR@yahoo.com', '2023-02-15');
+SELECT *
+FROM course_enrollment.students.profiles;
+
+
+
